329d9053374843a4d2db5f567e3bcb3bfef126a3
pratik jambhulkar <pratikj009@gmail.com>
2020-05-03 17:28:07 +0530

    Add mysql integration

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
-	"math/rand"
+	"io/ioutil"
 	"net/http"
-	"strconv"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
@@ -16,8 +17,16 @@ type Student struct {
 }
 
 var students []Student
+var db *sql.DB
+var err error
 
 func main() {
+	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/students")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
 	router := mux.NewRouter()
 
 	students = append(students, Student{ID: "1", Name: "Namita", Standard: "First"})
@@ -34,55 +43,105 @@ func main() {
 
 func getStudents(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var students []Student
+
+	result, err := db.Query("SELECT id, name, standard from student;")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	for result.Next() {
+		var student Student
+		err := result.Scan(&student.ID, &student.Name, &student.Standard)
+		if err != nil {
+			panic(err.Error())
+		}
+		students = append(students, student)
+	}
 	json.NewEncoder(w).Encode(students)
 }
 
 func getStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range students {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
+
+	result, err := db.Query("SELECT id, name, standard FROM student WHERE id = ?;", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	var student Student
+	for result.Next() {
+		err := result.Scan(&student.ID, &student.Name, &student.Standard)
+		if err != nil {
+			panic(err.Error())
 		}
 	}
-	json.NewEncoder(w).Encode(&Student{})
+	json.NewEncoder(w).Encode(student)
 }
 
 func createStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var student Student
-	_ = json.NewDecoder(r.Body).Decode(&student)
-	student.ID = strconv.Itoa(rand.Intn(1000000))
-	students = append(students, student)
-	json.NewEncoder(w).Encode(&student)
+	stmt, err := db.Prepare("INSERT INTO student(id, name, standard) VALUES(?, ?, ?);")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	name := keyVal["name"]
+	standard := keyVal["standard"]
+	id := keyVal["id"]
+
+	_, err = stmt.Exec(id, name, standard)
+	if err != nil {
+		panic(err.Error())
+	}
+	json.NewEncoder(w).Encode(keyVal)
 }
 
 func updateStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range students {
-		if item.ID == params["id"] {
-			students = append(students[:index], students[index+1:]...)
-			var student Student
-			_ = json.NewDecoder(r.Body).Decode(&student)
-			student.ID = params["id"]
-			students = append(students, student)
-			json.NewEncoder(w).Encode(&student)
-			return
-		}
+
+	stmt, err := db.Prepare("UPDATE student SET name = ?, standard = ? WHERE id = ?;")
+	if err != nil {
+		panic(err.Error())
 	}
-	json.NewEncoder(w).Encode(students)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	newName := keyVal["name"]
+	newStd := keyVal["standard"]
+	keyVal["id"] = params["id"]
+	_, err = stmt.Exec(newName, newStd, params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	json.NewEncoder(w).Encode(keyVal)
 }
 
 func deleteStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range students {
-		if item.ID == params["id"] {
-			students = append(students[:index], students[index+1:]...)
-			break
-		}
+	stmt, err := db.Prepare("DELETE FROM student WHERE id = ?;")
+	if err != nil {
+		panic(err.Error())
 	}
-	json.NewEncoder(w).Encode(students)
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	keyVal["message"] = "Student with ID = " + params["id"] + " was deleted"
+	json.NewEncoder(w).Encode(keyVal)
 }
